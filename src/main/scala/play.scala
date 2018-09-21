d20b3acbdf6808d8a814e3a8a1682bbf1f803868
Roland4444 <turbopollux@rambler.ru>
2018-09-21 16:34:05 +0000

    experiment by placeholder

--- src/main/scala/play.scala
+++ src/main/scala/play.scala
@@ -2,29 +2,23 @@ object play extends App{
 
   import org.openqa.selenium.By
   import org.openqa.selenium.WebDriver
-  import org.openqa.selenium.WebElement
   import org.openqa.selenium.firefox.FirefoxDriver
   import org.openqa.selenium.support.ui.ExpectedCondition
   import org.openqa.selenium.support.ui.WebDriverWait
 
   val driver = new FirefoxDriver
-
-  // And now use this to visit Google
-  driver.get("https://ras.arbitr.ru/")
-  // Alternatively the same thing can be done like this
-  // driver.navigate().to("http://www.google.com");
-
-  // Find the text input element by its name
-  val element = driver.findElement(By.name("q"))
-
+  driver get("https://ras.arbitr.ru/")
+  val element = driver findElementByXPath("//*[text()='Найти']")
+  val holder = driver findElementByXPath("//input[@placeholder='название, ИНН или ОГРН']")
   // Enter something to search for
-  element.sendKeys("Cheese!")
+  holder sendKeys("3015011755")
+  element click
 
   // Now submit the form. WebDriver will find the form for us from the element
-  element.submit()
+  //element.submit()
 
   // Check the title of the page
-  System.out.println("Page title is: " + driver.getTitle)
+  println("Page title is: " + driver.getTitle)
 
   // Google's search is rendered dynamically with JavaScript.
   // Wait for the page to load, timeout after 10 seconds
